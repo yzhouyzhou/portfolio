f40ab03f9cbf494f4825260b130bfc5beb06f480
Ying Zhou <yzhouyzhou@yahoo.com>
2019-10-01 12:08:59 -0500

    modify

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,8 +6,14 @@ module.exports = function (app) {
     app.get("/Resume", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/assets/images/ying-zhou-resume.pdf"));
     });
-
-    app.get("*", function (req, res) {
-        res.render("index", { projects });
+    app.get("/Games", function (req, res) {
+        res.render("games", { projects });
     });
+    app.get("/Portfolio", function (req, res) {
+        res.render("portfolio");
+    });
+    app.get("*", function (req, res) {
+        res.render("index");
+    });
+   
 };
